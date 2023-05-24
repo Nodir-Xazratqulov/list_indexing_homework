427374d5522ffc78617bd7ca423f73908aeffd3b
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-24 19:11:44 +0500

    list10

--- List10.py
+++ List10.py
@@ -6,4 +6,23 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return
+   
+    # return max(list_num[0],list_num[-1])
+    i=0
+    max=0
+
+    while i<len(list_num):
+        
+        if list_num[i]>max:
+            max=list_num[i]
+        i+=1
+    list_num.remove(max)
+    j=0
+    max1=0
+    while j<len(list_num):
+        
+        if list_num[j]>max1:
+            max1=list_num[j]
+        j+=1
+    return max1
+print(main([12, 32, 1, 4, 33]))
